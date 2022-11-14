65360e3e69066f292cbb16a640ba84fc37afa5eb
Max Zavalo <max.mine2104@gmail.com>
2022-11-15 00:34:43 +0300

    task1

--- src/HomeworkArrays.java
+++ src/HomeworkArrays.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class HomeworkArrays {
     public static void main(String[] args) {
+        task1();
+    }
 
+    private static void task1() {
+        String[] names = new String[]{"Max", "Nikita", "Roma", "Andrey", "Denis"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
     }
 }
